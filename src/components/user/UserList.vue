e713ff49fd5ed8b6f4749ebb48b76a918ea4eca2
SovikPC <vital.gurov2017@gmail.com>
2023-10-07 21:58:22 +0800

    add UserList and UserListView.vue

---
+++ src/components/user/UserList.vue
@@ -0,0 +1,52 @@
+<script>
+import DataService from '@/services/DataService'
+
+export default {
+  name: 'users-data',
+  data () {
+    return {
+      num: 1,
+      users: []
+    }
+  },
+  methods: {
+    getAllUser () {
+      DataService.getAllUser().then((response) => {
+        this.users = response.data
+      })
+    }
+  },
+  created () {
+    this.getAllUser()
+  }
+}
+</script>
+
+<template>
+<table class="table table-striped">
+  <thead>
+  <tr>
+    <th>№</th>
+    <th>User last name</th>
+    <th>User first name</th>
+    <th>User father name</th>
+    <th>User login</th>
+    <th>User role</th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr v-for="user in users" v-bind:key="user.id">
+    <td>{{num++}}</td>
+    <td>{{user.last_name}}</td>
+    <td>{{user.first_name}}</td>
+    <td>{{user.father_name}}</td>
+    <td>{{user.login}}</td>
+    <td>{{user.roles.name}}</td>
+  </tr>
+  </tbody>
+</table>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/user/UserListView.vue
@@ -0,0 +1,20 @@
+<script>
+import UserList from '@/components/user/UserList.vue'
+
+export default {
+  name: 'UserListView',
+  components: {
+    UserList
+  }
+}
+</script>
+
+<template>
+<div class="container">
+  <UserList/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
